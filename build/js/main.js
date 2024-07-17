285d16b8c6c95678bd0a528e11f5c7c251da721e
y/h0R9i8Db! <agustingomez999js@gmail.com>
2024-07-17 20:16:05 -0300

    primer commit

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
-
-
+    swiper()
+    parallax()
 });
 
 
@@ -16,4 +16,25 @@ function swiper(){
     });
 }
 
-swiper()
+const parallax = () => {
+    const h1 = document.querySelector('h1');
+    let translate = 0;
+    let lastScrollTop = 0;
+
+    document.addEventListener('scroll', () => {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop < lastScrollTop) {
+            // Si el scroll está subiendo, resetea translate a 0
+            translate = 0;
+        } else {
+            // Si el scroll está bajando, incrementa translate
+            translate += 5;
+        }
+
+        console.log(translate);
+        h1.style.transform = `translateY(${translate}px)`;
+
+        lastScrollTop = scrollTop;
+    });
+}
