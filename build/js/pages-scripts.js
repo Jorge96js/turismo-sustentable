b07ff2570a8b0b1a40eb37b75cafb96becd02516
y/h0R9i8Db! <agustingomez999js@gmail.com>
2024-09-09 19:27:41 -0300

    ;)

--- build/js/pages-scripts.js
+++ build/js/pages-scripts.js
@@ -1,19 +1,40 @@
-const params = new URLSearchParams(window.location.search);
-const id = parseInt(params.get('id'));
-const h1 = document.getElementById('formPage-title');
+document.addEventListener('DOMContentLoaded', function(){
+    pageSwiper();
+    pagesTitles();
+})
+
+function pagesTitles(){
+    const params = new URLSearchParams(window.location.search);
+    const id = parseInt(params.get('id'));
+    const h1 = document.getElementById('formPage-title');
 
 
-switch (id) {
-    case 0:
-        h1.innerText = 'Reserva un hospedaje a Mendoza';
-        break;
-    case 1:
-        h1.innerText = 'Reserva un hospedaje a Misiones';
-        break;
-    case 2:
-        h1.innerText = 'Reserva un hospedaje a Santa Cruz';
-        break;
-    default:
-        h1.innerText = 'Reserva un hospedaje a tu destino';
-        break;
+    switch (id) {
+        case 0:
+            h1.innerText = 'Reserva un hospedaje a Mendoza';
+            break;
+        case 1:
+            h1.innerText = 'Reserva un hospedaje a Misiones';
+            break;
+        case 2:
+            h1.innerText = 'Reserva un hospedaje a Santa Cruz';
+            break;
+        default:
+            h1.innerText = 'Reserva un hospedaje a tu destino';
+            break;
+    }
 }
+
+function pageSwiper(){
+    const swiper = new Swiper('.pages-swiper', {
+        direction: 'vertical',
+        loop: true,
+        autoplay: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+    });
+  
+}
+
